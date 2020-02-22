9f59cb3db28125f2ab42f4eba1f0673a8afa1fe4
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-02-22 12:28:08 +0300

    error showing added

--- lab1/task1/replace/replace/Source.cpp
+++ lab1/task1/replace/replace/Source.cpp
@@ -6,13 +6,19 @@ using namespace std;
 #include <stdio.h>
 #include <optional>
 
-
 struct Args
 {
 	string inputFileName;
 	string outputFileName;
 	string searchString;
 	string replaceString;
+};
+
+enum Error {
+	TOO_FEW_ARGUMENTS,
+	INPUT_FILE_OPENING_ERROR,
+	OUTPUT_FILE_OPENING_ERROR,
+	THERE_IS_NOT_ERROR,
 };
 
 optional<Args> ParseArguments(int argc, char** argv)
@@ -63,14 +69,37 @@ void CopyFileWithReplace(std::istream& input, std::ostream& output,
 	}
 }
 
+
+void ShowEror(Error error, optional<Args> args)
+{
+	if (error == TOO_FEW_ARGUMENTS)
+	{
+		cout << "Invalid argument count\n";
+		cout << "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		return;
+	}
+
+	if (error == INPUT_FILE_OPENING_ERROR)
+	{
+		cout << "Failed to open " << args->inputFileName << " for reading" << endl;
+		return;
+	}
+
+	if (error == OUTPUT_FILE_OPENING_ERROR)
+	{
+		cout << "Failed to open " << args->outputFileName << " for writing" << endl;
+		return;
+	}
+}
+
+
 int main(int argc, char** argv)
 {
 	optional args = ParseArguments(argc, argv);
 
 	if (!args)
 	{
-		cout << "Invalid argument count\n";
-		cout <<"Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		ShowEror(TOO_FEW_ARGUMENTS, args);
 		return 1;
 	}
 
@@ -82,19 +111,18 @@ int main(int argc, char** argv)
 
 	if (!inputFile.is_open())
 	{
-		cout << "Failed to open " << args->inputFileName << " for reading" << endl;
+		ShowEror(INPUT_FILE_OPENING_ERROR, args);
 		return 1;
 	}
 
 	if (!outputFile.is_open())
 	{
-		cout << "Failed to open " << args->outputFileName << " for w" << endl;
+		ShowEror(OUTPUT_FILE_OPENING_ERROR, args);
 		return 1;
 	}
 
 	string search = args->searchString;
 	string replace = args->replaceString;
-
 
 	CopyFileWithReplace(inputFile, outputFile, search, replace);
 	outputFile.flush();
