c97856695d9d59fe1102ce5d292e323a92c11b76
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-03-16 15:28:22 +0300

    Исправлены тесты

--- lab1/task3/invert/invert/test.bat
+++ lab1/task3/invert/invert/test.bat
@@ -8,42 +8,40 @@ if %MyProgram%=="" (
 )
 
 REM Недостаточное количество аргументов
-%MyProgram% > "%TEMP%\out.txt" && goto err
-fc test\too_few_arguments.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% > "%TEMP%\out.txt" && goto err1
 echo test 1 passed successfully
 
 REM Нечисловая матрица
-%MyProgram% "test/invalid_matrix_elements_input.txt" > "%TEMP%\out.txt" && goto err
-fc test\invalid_matrix_elements_output.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% "test/invalid_matrix_elements_input.txt" > "%TEMP%\out.txt" && goto err1
 echo test 2 passed successfully
 
 REM Некорректное имя входного файла
-%MyProgram% "test/input.xtx" > "%TEMP%\out.txt" && goto err
-fc test\invalid_file_name.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% "test/input.xtx" > "%TEMP%\out.txt" && goto err1
 echo test 3 passed successfully
 
 REM Некорректный размер матрицы
-%MyProgram% "test/invalid_matrix_size_input.txt" > "%TEMP%\out.txt" && goto err
-fc test\invalid_matrix_size_output.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% "test/invalid_matrix_size_input.txt" > "%TEMP%\out.txt" && goto err1
 echo test 4 passed successfully
 
 REM Матрица нулей
-%MyProgram% "test/zero_matrix_input.txt" > "%TEMP%\out.txt" && goto err
-fc test\zero_matrix_output.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% "test/zero_matrix_input.txt" > "%TEMP%\out.txt" && goto err1
 echo test 5 passed successfully
 
 REM Единичная матрица
-%MyProgram% "test/unit_matrix_input.txt" > "%TEMP%\out.txt" && goto err
-fc test\unit_matrix_output.txt "%TEMP%\out.txt" || goto err2
+%MyProgram% "test/unit_matrix_input.txt" > "%TEMP%\out.txt" && goto err1
 echo test 6 passed successfuly
 
-%MyProgram% "test/matrix1_input.txt" > "%TEMP%\out.txt" || goto err
-fc test\matrix1_output.txt "%TEMP%\out.txt" && goto err2
+%MyProgram% "test/matrix1_input.txt" > "%TEMP%\out.txt" || goto err1
+fc "%TEMP%\out.txt" test\matrix1_output.txt || goto err2
 echo test 7 passed successfully
 
-%MyProgram% "test/matrix2_input.txt" > "%TEMP%\out.txt" || goto err
-fc test\matrix2_output.txt "%TEMP%\out.txt" && goto err2
+%MyProgram% "test/matrix2_input.txt" > "%TEMP%\out.txt" || goto err1
+fc test\matrix2_output.txt "%TEMP%\out.txt" || goto err2
 echo test 8 passed successfully
+
+%MyProgram% "test/matrix3_input.txt" > "%TEMP%\out.txt" || goto err1
+fc test\matrix3_output.txt "%TEMP%\out.txt" || goto err2
+echo test 9 passed successfully
 
 echo all tests have passed successfully
 exit /B 0
