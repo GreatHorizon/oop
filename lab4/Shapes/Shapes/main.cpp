7861e4c328f140abdc8a582358c35b397839d083
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-05-19 10:00:34 +0300

    Added input prompt

--- lab4/Shapes/Shapes/main.cpp
+++ lab4/Shapes/Shapes/main.cpp
@@ -7,8 +7,15 @@ int main()
 {
 	ShapesControl control(cin, cout);
 
+	cout << "Commands:\n"
+		<< "line <first point X> <first point Y> <second point X> <second point Y> <line color>\n"
+		<< "circle <center point X> <center Point Y> <outline color> <fill color>\n"
+		<< "rectangle <left top point X> <left top point Y> <width> <height> <outline color> <fill color>\n"
+		<< "triangle <vertex1 X> <vertex1 Y> <vertex2 X> <vertex2 Y> <vertex3 X> <vertex3 Y> <outline color> <fill color>\n\n";
+
 	while (!cin.eof() && !cin.fail())
 	{
+		cout << ">";
 		if (!control.HandleCommand())
 		{
 			cout << "Incorrect command\n";
